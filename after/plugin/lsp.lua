ca7de3404f508343c71fb1d671233847bda68d66
Pedro Coelho <punnie@ethereal.io>
2023-11-22 17:55:28 +0000

    Add sign icons to LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -6,6 +6,13 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
+lsp_zero.set_sign_icons({
+  error = '✘',
+  warn = '▲',
+  hint = '⚑',
+  info = '»'
+})
+
 -- Language servers
 require('lspconfig').lua_ls.setup({})
 require('lspconfig').solargraph.setup({})
