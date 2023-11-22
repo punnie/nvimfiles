7179363b9d40398bbfea76e216583d1dd3b0ca44
Pedro Coelho <punnie@ethereal.io>
2023-11-22 10:29:48 +0000

    Get neogit to work in a vsplit

--- after/plugin/neogit.lua
+++ after/plugin/neogit.lua
@@ -2,4 +2,4 @@ local neogit = require('neogit')
 
 neogit.setup {}
 
-vim.keymap.set('n', '<leader>gg', function() neogit.open({ kind = 'split' }) end)
+vim.keymap.set('n', '<leader>gg', function() neogit.open({ kind = 'vsplit' }) end)
