392b35e503f886e8bd9de7f3844f6153aeb74c4e
Pedro Coelho <punnie@ethereal.io>
2023-11-22 10:29:06 +0000

    Add useful keybindings to neotree

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -1,3 +1,5 @@
 require("nvim-tree").setup()
 
 vim.keymap.set('n', '<leader>op', vim.cmd.NvimTreeToggle)
+vim.keymap.set('n', '<leader>oP', vim.cmd.NvimTreeFindFile)
+vim.keymap.set('n', '<leader>OP', vim.cmd.NvimTreeFindFile)
